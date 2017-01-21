bb8d30068c06078ee1b0dc5c8fb011b07e38e78a
Clement Capart <clement.capart@gmail.com>
2017-01-21 21:47:28 +0000

    Parallax

---
+++ Assets/Code/Graphics/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour 
+{
+	private List<ParallaxSprite> m_ParallaxSprites = new List<ParallaxSprite>();
+	private Vector3 m_lastFramePosition = Vector3.zero;
+
+	void Start()
+	{
+		m_ParallaxSprites = new List<ParallaxSprite>(FindObjectsOfType<ParallaxSprite>());
+		m_lastFramePosition = transform.position;
+	}
+
+	void LateUpdate()
+	{
+		for(int i = 0; i < m_ParallaxSprites.Count; i++)
+		{
+			Vector3 parallaxPosition = (m_lastFramePosition - transform.position) * (m_ParallaxSprites[i].m_ParallaxSpeed - 1.0f) ;
+			m_ParallaxSprites[i].transform.position += new Vector3(parallaxPosition.x, parallaxPosition.y, 0.0f);
+		}
+
+		m_lastFramePosition = transform.position;
+	}
+}
---
+++ Assets/Code/Graphics/ParallaxSprite.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxSprite : MonoBehaviour 
+{
+	[Range(0.0f, 2.0f)]
+	public float m_ParallaxSpeed = 1.0f;
+}
