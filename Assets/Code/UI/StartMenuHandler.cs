daa62b87088daf36fef1877d439cfc8bc1560611
Clement Capart <clement.capart@gmail.com>
2017-02-12 16:26:36 +0000

    Sorry it's ugly.

--- Assets/Code/UI/StartMenuHandler.cs
+++ Assets/Code/UI/StartMenuHandler.cs
@@ -18,6 +18,9 @@ public class StartMenuHandler : MonoBehaviour
 
 	public List<Graphic> m_PressAnyKeyImages = null;
 	public List<Graphic> m_CharacterSelectImages = null;
+	public Graphic m_PanthodeoImage = null;
+	public Graphic m_ZiggyImage = null;
+	public Graphic m_StartGameGraphic = null;
 
 	void Start()
 	{
@@ -31,6 +34,10 @@ public class StartMenuHandler : MonoBehaviour
 		{
 			m_CharacterSelectImages[i].CrossFadeAlpha(0.0f, 0.0f, false);
 		}
+
+		m_ZiggyImage.CrossFadeAlpha(0.0f, 0.0f, false);
+		m_PanthodeoImage.CrossFadeAlpha(0.0f, 0.0f, false);
+		m_StartGameGraphic.CrossFadeAlpha(0.0f, 0.0f, false);
 
 		RequestState(StartMenuState.PressAnyKey);
 	}
@@ -63,6 +70,9 @@ public class StartMenuHandler : MonoBehaviour
 
 			case StartMenuState.SelectCharacter:
 				GameSessionData.s_CurrentCharacters = Characters.None;
+
+				m_PanthodeoImage.CrossFadeAlpha(0.3f, 0.0f, false);
+				m_ZiggyImage.CrossFadeAlpha(0.3f, 0.0f, false);
 				for (int i = 0; i < m_PressAnyKeyImages.Count; i++)
 				{
 					m_PressAnyKeyImages[i].CrossFadeAlpha(0.0f, 0.5f, false);
@@ -94,12 +104,28 @@ public class StartMenuHandler : MonoBehaviour
 			case StartMenuState.SelectCharacter:
 				if (Input.GetKeyDown(KeyCode.Space) || XInput.GetButtonDown(Buttons.A, 0))
 				{
-					GameSessionData.s_CurrentCharacters |= Characters.Panthodeo;
+					if ((GameSessionData.s_CurrentCharacters & Characters.Panthodeo) == 0)
+					{
+						if (GameSessionData.s_CurrentCharacters == 0)
+						{
+							m_StartGameGraphic.CrossFadeAlpha(1.0f, 0.5f, false);
+						}
+						GameSessionData.s_CurrentCharacters |= Characters.Panthodeo;
+						m_PanthodeoImage.CrossFadeAlpha(1.0f, 0.5f, false);
+					}
 				}
 
 				if (Input.GetKeyDown(KeyCode.RightControl) || XInput.GetButtonDown(Buttons.A, 1))
 				{
-					GameSessionData.s_CurrentCharacters |= Characters.Ziggy;
+					if ((GameSessionData.s_CurrentCharacters & Characters.Ziggy) == 0)
+					{
+						if(GameSessionData.s_CurrentCharacters == 0)
+						{
+							m_StartGameGraphic.CrossFadeAlpha(1.0f, 0.5f, false);
+						}
+						GameSessionData.s_CurrentCharacters |= Characters.Ziggy;
+						m_ZiggyImage.CrossFadeAlpha(1.0f, 0.5f, false);
+					}
 				}
 
 				if(GameSessionData.s_CurrentCharacters != 0 && (Input.GetKeyDown(KeyCode.Return) || XInput.GetButtonDown(Buttons.Start, 0) || XInput.GetButtonDown(Buttons.Start, 1)))
@@ -131,7 +157,6 @@ public class StartMenuHandler : MonoBehaviour
 	
 	private void RequestStartGame()
 	{
-		m_State = StartMenuState.Launching;
 		FadeScreen.FadeToBlack(3.0f, StartGame);
 	}
 
