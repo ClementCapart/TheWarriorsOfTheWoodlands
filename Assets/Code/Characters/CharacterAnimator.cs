0590a0386c7d990d884426552d7e21e2a05c0ba9
Clement Capart <clement.capart@gmail.com>
2017-02-26 21:59:06 +0000

    Hurt animation

--- Assets/Code/Characters/CharacterAnimator.cs
+++ Assets/Code/Characters/CharacterAnimator.cs
@@ -6,6 +6,7 @@ public class CharacterAnimator : MonoBehaviour
 {
 	private CharacterDataModule m_Character = null;
 	public bool m_HasAlreadyAttacked = false;
+	public bool m_HasAlreadyBeenHurt = false;
 	public bool m_LoopAttack = false;
 	private Animator m_Animator = null;
 
@@ -17,31 +18,46 @@ public class CharacterAnimator : MonoBehaviour
 
 	private void Update()
 	{
-		if (m_Character.State == CharacterState.Default)
+		if (m_Character.State != CharacterState.Dead)
 		{
-			if (m_Character.IsMoving)
+
+			if (m_Character.State == CharacterState.Hitlocked)
 			{
-				m_Animator.SetBool("Walk", true);
+				if (!m_HasAlreadyBeenHurt)
+				{
+					m_Animator.SetTrigger("Hurt");
+				}
+				m_HasAlreadyBeenHurt = true;
+				
 			}
 			else
 			{
-				m_Animator.SetBool("Walk", false);
-			}
+				m_HasAlreadyBeenHurt = false;
 
-			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * m_Character.Direction, transform.localScale.y,
-				transform.localScale.z);
+				if (m_Character.IsMoving)
+				{
+					m_Animator.SetBool("Walk", true);
+				}
+				else
+				{
+					m_Animator.SetBool("Walk", false);
+				}
 
-			if (m_Character.IsAttacking && (m_LoopAttack || !m_HasAlreadyAttacked))
-			{
-				m_HasAlreadyAttacked = true;
-				m_Animator.SetTrigger("Attack");
-			}
-			else if (!m_Character.IsAttacking)
-			{
-				m_HasAlreadyAttacked = false;
-			}
+				transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * m_Character.Direction, transform.localScale.y,
+					transform.localScale.z);
+
+				if (m_Character.IsAttacking && (m_LoopAttack || !m_HasAlreadyAttacked))
+				{
+					m_HasAlreadyAttacked = true;
+					m_Animator.SetTrigger("Attack");
+				}
+				else if (!m_Character.IsAttacking)
+				{
+					m_HasAlreadyAttacked = false;
+				}
+			}			
 		}
-		else if(m_Character.State == CharacterState.Dead)
+		else if (m_Character.State == CharacterState.Dead)
 		{
 			if (!m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
 			{
