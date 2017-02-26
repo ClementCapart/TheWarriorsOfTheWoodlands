dee8dd1b4ea2617453e49c546e9a13f91d3c48b2
Clement Capart <clement.capart@gmail.com>
2017-02-26 18:00:48 +0000

    Credits

---
+++ Assets/Code/Utils/CreditsUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsUtils : MonoBehaviour 
+{
+	public void FadeFromBlack()
+	{
+		FadeScreen.FadeFromBlack(0.5f);
+	}
+
+	public void GotoMainMenu()
+	{
+		FadeScreen.FadeToBlack(0.5f, LoadMainMenu);
+	}
+
+	void LoadMainMenu()
+	{
+		SceneManager.LoadScene("StartMenu");
+	}
+}
